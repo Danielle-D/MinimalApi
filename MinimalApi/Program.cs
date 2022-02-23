bc5645201957b9316de7d9d29130ec48f8325b2b
Danielle <43252788+Danielle-D@users.noreply.github.com>
2022-02-23 21:23:15 +0000

    Add files via upload

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapPost("/fileName", (IFormFile file, CancellationToken cancellationToken) =>
+{
+    return file.FileName;
+})
+.WithName("GetFileName");
+
+app.MapPost("/fileExtension", (IFormFile file, CancellationToken cancellationToken) =>
+{
+    return Path.GetExtension(file.FileName);
+})
+.WithName("GetFileExtension");
+
+app.MapPost("/uploadContent", async (IFormFile file) =>
+{
+    using var stream = System.IO.File.OpenWrite("upload.txt");
+    await file.CopyToAsync(stream);
+})
+.WithName("UploadContent");
+
+app.Run();
